cbdc0cf6eb0f96072b0cb99c498b488d7cd25adc
vikas <96467900+vikascodesoftic455@users.noreply.github.com>
2022-01-18 17:49:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
+const functions = require("firebase-functions");
 const express = require('express')
-const dbconn = require('./DB/db')
+const dbconn = require('./db/db')
 const app =express()
 const router = require('./router/Index')
+const path = require("path");
+const http = require("http");
 const customerrouter = require('./router/customer')
 const res = require('express/lib/response')
 const port =process.env.PORT || 3400
@@ -15,15 +18,31 @@ app.use(express.json())
 //  }
 
 //  app.use(logger)
-app.get('/',()=>{
-    res.send("<h1>hellovikad</h1>")
-})
+
 app.use('/api',router)
 app.use('/api/customer',customerrouter)
 
-app.listen(port,()=>{
-    console.log(`Server is running 8000`)
-})
+const environment = process.env.NODE_ENV;
+
+
+ app.listen(port,()=>{
+     console.log(`Server is running ${port}`)
+     if (
+        environment !== "production" &&
+        environment !== "development" &&
+        environment !== "testing"
+      ) {
+        console.error(
+          `NODE_ENV is set to ${environment}, but only production and development are valid.`
+        );
+        process.exit(1);
+      }
+ })
+
+
+
+
+exports.app = functions.https.onRequest(app);
 
 
 
